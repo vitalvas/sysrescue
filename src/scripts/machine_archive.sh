25bfdf267c2442e10fca93fb7273818550214bc3
Vitaliy Vasilenko <me@vitalvas.com>
2022-04-17 16:19:34 +0300

    add machine_archive script

---
+++ src/scripts/machine_archive.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+ARCHIVE_PATHS=(
+  /etc
+  /opt
+  /srv
+  /media
+  /home
+  /root
+  /var/backups
+  /var/www
+  /var/spool
+  /var/lib
+  /usr/local/bin
+  /usr/local/sbin
+)
+
+HOSTNAME=$(hostname -f)
+
+if [ -e "/usr/bin/dpkg" ]; then
+  /usr/bin/dpkg --get-selections > /var/backups/dpkg-get-selections.txt
+  /usr/bin/dpkg --list > /var/backups/dpkg-list.txt
+fi
+
+for path in $(ls /usr/src/ | grep -v 'linux-headers'); do
+  ARCHIVE_PATHS+=("/usr/src/${path}")
+done
+
+current_archive_paths=""
+
+for path in ${ARCHIVE_PATHS[*]}; do
+  if [ -d "${path}" ]; then
+    current_archive_paths="${current_archive_paths} ${path}"
+  fi
+done
+
+filename=${HOSTNAME}-$(date +"%Y-%m-%d").tar.zst
+
+if [ -f "/${filename}" ]; then
+  rm /${filename}
+fi
+
+tar --zstd -cf /${filename} ${current_archive_paths}
+
