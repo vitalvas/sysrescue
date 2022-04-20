052d3af0da215060bb533feb8629fd19a8169097
Vitaliy Vasilenko <me@vitalvas.com>
2022-04-20 22:19:00 +0300

    fix missing hosts file

--- src/scripts/install_os_ubuntu.sh
+++ src/scripts/install_os_ubuntu.sh
@@ -179,6 +179,19 @@ auto eth0
 iface eth0 inet dhcp
 EOD
 
+cat <<EOD>/etc/hosts
+127.0.0.1 localhost
+#127.0.1.1 server-name
+
+# The following lines are desirable for IPv6 capable hosts
+::1 ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+EOD
+
 mkdir -p /root/.ssh/
 chmod 700 /root/.ssh/
 
