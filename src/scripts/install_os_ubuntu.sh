64b8b4871f89dab260731e6347f6b6e3cb74d328
Vitaliy Vasilenko <me@vitalvas.com>
2022-04-20 16:57:44 +0300

    add install bash-completion

--- src/scripts/install_os_ubuntu.sh
+++ src/scripts/install_os_ubuntu.sh
@@ -148,7 +148,7 @@ fi
 apt install -qy --no-install-recommends ${pkg_install} grub-common grub-efi-amd64 grub-pc-bin systemd initramfs-tools \
 	dbus-user-session systemd-sysv init init-system-helpers lsb-release isc-dhcp-client mdadm \
 	ifupdown ethtool iputils-ping net-tools openssh-server iproute2 vim util-linux locales less wget curl dnsutils ntp \
-	rsyslog
+	rsyslog bash-completion
 
 locale-gen en_US.UTF-8
 
