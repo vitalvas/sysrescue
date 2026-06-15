410985e01d844afd725447730b9ada0297eb0977
Vitaliy Vasylenko <me@vitalvas.com>
2026-06-15 07:30:22 -0400

    fix: grub-install for efi

--- src/scripts/install_os_ubuntu_efi.sh
+++ src/scripts/install_os_ubuntu_efi.sh
@@ -208,9 +208,9 @@ systemctl set-default multi-user.target
 
 update-initramfs -u
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --boot-directory=/boot/efi/EFI --removable --bootloader-id=grub
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
 
-grub-mkconfig -o /boot/efi/EFI/grub/grub.cfg
+update-grub
 
 echo -e "root123\nroot123" | passwd root
 
